3385e4bcf6d7b2860554b0680e5753e1af06abd3
Mustafa <mmj48@users.sourceforge.net>
2011-12-23 15:56:57 -0500

    location = "#header" when opening editor

--- pafm/pafm/js.js
+++ pafm/pafm/js.js
@@ -454,7 +454,7 @@ edit = {
 				obj.attributes.selected = "true";
 			tempAr.push("option", obj);
 		}
-		(ll = $("ll")).innerHTML = "";
+		(ll = $("ll")).innerHTML = ""; //language list
 		json2markup(tempAr, ll);
 		ajax("?do=readFile&path=" + path + "&subject=" + subject, "get", null, function(response){
 			$("ta").value = response;
@@ -481,6 +481,7 @@ edit = {
 			else
 				CodePress.run();
 		});
+		location = "#header";
 	},
 	save : function(subject, path){
 		$("editMsg").innerHTML = null;
